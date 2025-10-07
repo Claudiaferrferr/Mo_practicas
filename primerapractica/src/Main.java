f91e27bcad6bb172dc25a65933b18617eb08c53f
Claudiaferrferr <claudiaferranferrer003@gmail.com>
2025-10-07 19:58:50 +0200

    Add files via upload

---
+++ primerapractica/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esta impresion la acabo de havcer");
+        //aclaracion de comentario
+        String nombreCompleto = "Borja m.h";
+        final String numeroDNI = "123J";
+        System.out.println("Mi nombre es "+nombreCompleto);
+        int edad = 41;
+        float altura = 1.71f;
+        double peso = 70.243534;
+        char letraCarnet = 'j';
+        boolean experiencia = true;
+        Object cosa = true;
+        cosa = 2;
+        System.out.println("Miedad es " + edad + "kg");
+        System.out.println("Miedad es " + altura);
+        System.out.println("Miedad es " + peso);
+        System.out.println("Miedad es " + letraCarnet);
+        System.out.println("Tengo experiencia" + experiencia);
+        System.out.println("Mi numero de dni es " + numeroDNI);
+        }
+
+    }
+
+
